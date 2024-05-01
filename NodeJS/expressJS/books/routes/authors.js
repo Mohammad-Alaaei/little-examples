e3cbb6720af6394a4442ffc0cd353805bbf8975d
s.Mohammad v.Alaaei <s.mohammad.alaaei@gmail.com>
2024-05-01 16:12:38 +0330

    completed books and authors error and comfirm messages. now can just search books and authors. also some changes to books and authors pages.

--- NodeJS/expressJS/books/routes/authors.js
+++ NodeJS/expressJS/books/routes/authors.js
@@ -2,35 +2,62 @@ const express = require("express");
 const router = express.Router();
 const Author = require("../models/author");
 
+// Loading authors list in async. way
 router.get("/", async (req, res) => {
     try {
-        const authors = await Author.find({});
-        res.render("./authors/authors", { authors: authors });
+        const replaceExp = new RegExp("[?:/\~!@#$%^&*()_+<>;\"'.]", 'g');
+        let searchOptions = {}
+
+        if (req.query.name && req.query.name != '') {
+            req.query.name = req.query.name.replace(replaceExp, "");
+            searchOptions.name = new RegExp(req.query.name, 'i');
+        }
+
+        if (req.query.age && req.query.age != '') {
+            req.query.age = req.query.age.replace(replaceExp, "");
+            searchOptions.age = req.query.age;
+        }
+
+        const authors = await Author.find(searchOptions);
+
+        // check if there is any Query about adding a author fro 'Add author' page.
+        const authorAdded = req.query.authorAdded === 'true' ? 'Author added successfully.' : null;
+
+        res.render("./authors/authors", {
+            authors: authors,
+            confirmMessage: authorAdded,
+            searchOptions: req.query
+        });
+
     } catch {
         console.log("Something went wrong during the loading of the authors List. redirecting to home page.");
         res.redirect("/");
     }
 });
 
-router.post("/", (req, res) => {
+// save new author in database
+router.post("/", async (req, res) => {
+    // extract input data from form
     const { name, age } = req.body;
-
     const author = new Author({
         name,
         age
     });
-    
-    author.save().then((newAuthor) => {
-        console.log(`Author #${newAuthor._id} Added Successfuly.`);
-        // res.redirect(`authors/${newAuthor._id}`);
 
-        res.redirect("authors");
-    }).catch(() => {
-        res.render("authors/new", {
-            author: author,
-            errorMessage: "Faild to add the Author."
+    // try to save new author to database.
+    await author.save()
+        .then((newauthor) => {
+            console.log(`Author #${newauthor._id} Added Successfully.`);
+            // res.redirect(`authors/${newauthor._id}`);
+
+            res.redirect("authors?authorAdded=true");
+
+        }).catch(() => {
+            res.render("authors/new", {
+                author: author,
+                errorMessage: "Failed to add the author."
+            });
         });
-    });
 
 });
 
@@ -40,13 +67,13 @@ router.get("/new", (req, res) => {
 
 router.route("/:id")
     .get((req, res) => {
-        res.send(`Author ID: ${req.params.id}`);
+        res.send(`author ID: ${req.params.id}`);
     })
     .put((req, res) => {
-        res.send(`Update Author ID: ${req.params.id}`);
+        res.send(`Update author ID: ${req.params.id}`);
     })
     .delete((req, res) => {
-        res.send(`Delete Author ID: ${req.params.id}`);
+        res.send(`Delete author ID: ${req.params.id}`);
     })
 
 module.exports = router;
--- NodeJS/expressJS/books/routes/books.js
+++ NodeJS/expressJS/books/routes/books.js
@@ -2,36 +2,68 @@ const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
 
+// Loading Books list in async. way
 router.get("/", async (req, res) => {
     try {
-        const books = await Book.find({});
-        res.render("./books/books", { books: books });
+        const replaceExp = new RegExp("[?:/\~!@#$%^&*()_+<>;\"'.]", 'g');
+        let searchOptions = {}
+
+        if (req.query.name && req.query.name != '') {
+            req.query.name = req.query.name.replace(replaceExp, "");
+            searchOptions.name = new RegExp(req.query.name, 'i');
+        }
+
+        if (req.query.author && req.query.author != '') {
+            req.query.author = req.query.author.replace(replaceExp, "");
+            searchOptions.author = new RegExp(req.query.author, 'i');
+        }
+
+        if (req.query.age && req.query.age != '') {
+            req.query.age = req.query.age.replace(replaceExp, "");
+            searchOptions.age = req.query.age;
+        }
+
+        const books = await Book.find(searchOptions);
+
+        // check if there is any Query about adding a book fro 'Add book' page.
+        const bookAdded = req.query.bookAdded === 'true' ? 'Book added successfully.' : null;
+
+        res.render("./books/books", {
+            books: books,
+            confirmMessage: bookAdded,
+            searchOptions: req.query
+        });
+
     } catch {
         console.log("Something went wrong during the loading of the Books List. redirecting to home page.");
         res.redirect("/");
     }
 });
 
-router.post("/", (req, res) => {
+// save new book in database
+router.post("/", async (req, res) => {
+    // extract input data from form
     const { name, author, age } = req.body;
-
     const book = new Book({
         name,
         author,
         age
     });
-    
-    book.save().then((newBook) => {
-        console.log(`Book #${newBook._id} Added Successfuly.`);
-        // res.redirect(`books/${newBook._id}`);
 
-        res.redirect("books");
-    }).catch(() => {
-        res.render("books/new", {
-            book: book,
-            errorMessage: "Faild to add the book."
+    // try to save new book to database.
+    await book.save()
+        .then((newBook) => {
+            console.log(`Book #${newBook._id} Added Successfully.`);
+            // res.redirect(`books/${newBook._id}`);
+
+            res.redirect("books?bookAdded=true");
+
+        }).catch(() => {
+            res.render("books/new", {
+                book: book,
+                errorMessage: "Failed to add the book."
+            });
         });
-    });
 
 });
 
